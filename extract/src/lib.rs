7fcd2152e71d5dddf73104df918ef8a9f97fbfdc
Nikita Syzrantsev <nikita.syzrantsev@bostongene.com>
2024-06-29 11:14:44 +0400

    Remove inefficient code

--- extract/src/lib.rs
+++ extract/src/lib.rs
@@ -1,9 +1,8 @@
 mod fastq;
 mod errors;
 mod barcode;
-mod pattern;
 
-use seq_io::fastq::Reader;
+use seq_io::fastq::{Reader, Record};
 use barcode::Barcode;
 
 
@@ -13,18 +12,21 @@ pub fn run(read1: String, read2: Option<String>, pattern: String) {
     let fastq_buf = fastq::read_fastq(&read1);
 
     let mut reader = Reader::new(fastq_buf);
-    
-    let updated_pattern = pattern::update_pattern(&pattern);
 
-    println!("Final updated pattern: {:?}", updated_pattern);
-
-    let barcode = Barcode::new(&updated_pattern).expect("REASON");
+    let barcode = Barcode::new(&pattern).expect("REASON");
 
     while let Some(record) = reader.next() {
         let record = record.expect("Error reading record");
-        let caps = barcode.match_read(&record).unwrap();
-        let (read_seq, read_qual, read_header) = Barcode::cut_from_read_seq("UMI", caps, &record).unwrap();
-        println!("{}\n{}\n+\n{}", read_header, read_seq, read_qual);
+        let caps = barcode.match_read(&record);
+       match caps {
+            Ok(capture) => {
+                let (read_seq, read_qual, read_header) = Barcode::cut_from_read_seq("UMI", capture, &record).unwrap();
+                println!("{}\n{}\n+\n{}", read_header, read_seq, read_qual);
+            },
+            Err(_) => {
+                println!("{}\n{}\n+\n{}", std::str::from_utf8(record.head()).unwrap(), std::str::from_utf8(record.seq()).unwrap(), std::str::from_utf8(record.qual()).unwrap())
+            }
+        };
     }
 
 }
