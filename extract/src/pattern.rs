910224552c4f9bdcc79baeea6f4789c6a860b8c7
Nikita Syzrantsev <nikita.syzrantsev@bostongene.com>
2024-08-18 16:35:01 +0400

    Add unit test for test_generate_sequences_with_pcr_errors

--- extract/src/pattern.rs
+++ extract/src/pattern.rs
@@ -2,15 +2,21 @@ use std::mem::size_of;
 
 use fancy_regex::Regex;
 
-const SUPPORTED_NUCLEOTIDES: &str = "ATGCN";
-const ADAPTER_PATTERN_REGEX: &str = r"(?i)(?<!\[)\b[ATGCN]+\b(?!\])";
+const FUZZY_CHARACTER: &str = ".";
+const ADAPTER_PATTERN_REGEX: &str = r"(?i)(?<!\[)\b[atgcn]+\b(?!\])";
 
-pub fn generate_sequences_with_pcr_errors(string: &str, error_num: &usize) -> Vec<String> {
-    if string.len() == 1 || *error_num == 0 {
-        return vec![string.to_string()];
+pub fn generate_sequences_with_pcr_errors(string: &str, errors_num: &usize) -> Vec<String> {
+    
+    if *errors_num == 0 {
+        return vec![string.to_string().to_ascii_uppercase()];
     }
+    
     if string.is_empty() {
         return Vec::new();
+    }
+    
+    if *errors_num >= string.len() {
+        return vec![FUZZY_CHARACTER.repeat(string.len())];
     }
 
     let num_chars = string.chars().count();
@@ -25,13 +31,13 @@ pub fn generate_sequences_with_pcr_errors(string: &str, error_num: &usize) -> Ve
     let upper: Vec<char> = string.chars().map(|c| c.to_ascii_uppercase()).collect();
 
     for permutation_mask in 0..=max_permutation_mask {
-        if permutation_mask.count_ones() as usize != num_chars - error_num {
+        if permutation_mask.count_ones() as usize != num_chars - errors_num {
             continue;
         }
         let mut s = String::new();
         for idx in 0..num_chars {
             if (permutation_mask & (1 << idx)) == 0 {
-                s.push_str(SUPPORTED_NUCLEOTIDES)
+                s.push_str(FUZZY_CHARACTER)
             } else {
                 s.push(upper[idx])
             }
@@ -63,4 +69,22 @@ pub fn create_fuzzy(pattern: &str, max_error: &usize) -> String {
 
     result.push_str(&pattern[last_end..]);
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use crate::pattern::generate_sequences_with_pcr_errors;
+
+    #[rstest]
+    #[case(vec!["."], "a", 1)]
+    #[case(vec!["A"], "a", 0)]
+    #[case(vec![], "", 1)]
+    #[case(vec!["AAA.", "AA.A", "A.AA", ".AAA"], "AAAA", 1)]
+    #[case(vec!["..."], "AAA", 3)]
+    #[case(vec!["..."], "AAA", 4)]
+    fn test_generate_sequences_with_pcr_errors(#[case] expected: Vec<&str>, #[case] pattern: &str, #[case] errors_num: usize) {
+        assert_eq!(expected, generate_sequences_with_pcr_errors(pattern, &errors_num));
+    }
 }
