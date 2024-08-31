fa24b9b5f836363f3f38fe78711bc662199d2d35
Nikita Syzrantsev <syzrantsev.n@yandex.ru>
2024-08-31 21:00:42 +0400

    Add doc tests for pattern.rs

--- barkit-extract/src/pattern.rs
+++ barkit-extract/src/pattern.rs
@@ -30,7 +30,7 @@ impl BarcodePattern {
     /// ```
     /// use barkit_extract::pattern::BarcodePattern;
     /// 
-    /// let barcode_pattern = BarcodePattern::new("^atgc(?<UMI>[ATGCN]{12})", &1).expect("REASON");
+    /// let barcode_pattern = BarcodePattern::new("^atgc(?<UMI>[ATGCN]{12})", &1).unwrap();
     /// 
     /// let sequences_with_errors = barcode_pattern.get_sequence_with_errors("ATGC");
     /// assert_eq!(vec!["ATG.", "AT.C", "A.GC", ".TGC"], sequences_with_errors);
@@ -84,9 +84,9 @@ impl BarcodePattern {
     /// ```
     /// use barkit_extract::pattern::BarcodePattern;
     /// 
-    /// let barcode_pattern = BarcodePattern::new("^atgc(?<UMI>[ATGCN]{12})", &1).expect("REASON");
+    /// let barcode_pattern = BarcodePattern::new("^atgc(?<UMI>[ATGCN]{12})", &1).unwrap();
     /// 
-    /// let pattern_with_pcr_errors = barcode_pattern.get_pattern_with_errors().expect("REASON");
+    /// let pattern_with_pcr_errors = barcode_pattern.get_pattern_with_errors().unwrap();
     /// assert_eq!("^(ATG.|AT.C|A.GC|.TGC)(?<UMI>[ATGCN]{12})", pattern_with_pcr_errors);
     /// ```
     pub fn get_pattern_with_errors(&self) -> Result<String, Error> {
@@ -109,26 +109,32 @@ impl BarcodePattern {
 }
 
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum BarcodeType {
+    /// Moleculare barcode (UMI)
     Umi,
+
+    /// Sample barcode
     Sample,
+
+    /// Cell barcode
     Cell,
 }
 
-impl fmt::Display for BarcodeType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let barcode_str = match self {
-            BarcodeType::Umi => "UMI",
-            BarcodeType::Sample => "SB",
-            BarcodeType::Cell => "CB",
-        };
-        write!(f, "{}", barcode_str)
-    }
-}
-
 impl BarcodeType {
-    fn get_barcode_type(name: &str) -> Result<Self, Error> {
+    /// Parses type of barcode
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use barkit_extract::pattern::BarcodeType;
+    /// use barkit_extract::error::Error::UnexpectedCaptureGroupName;
+    /// 
+    /// assert_eq!(BarcodeType::Umi, BarcodeType::parse_type("UMI").unwrap());
+    /// assert_eq!(BarcodeType::Sample, BarcodeType::parse_type("SB").unwrap());
+    /// assert_eq!(BarcodeType::Cell, BarcodeType::parse_type("CB").unwrap());
+    /// ```
+    pub fn parse_type(name: &str) -> Result<Self, Error> {
         match name {
             "UMI" => Ok(BarcodeType::Umi),
             "SB" => Ok(BarcodeType::Sample),
@@ -138,13 +144,35 @@ impl BarcodeType {
     }
 }
 
+impl fmt::Display for BarcodeType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match self {
+            BarcodeType::Umi => "UMI",
+            BarcodeType::Sample => "SB",
+            BarcodeType::Cell => "CB",
+        })
+    }
+}
+
+
 #[derive(Clone)]
 pub struct BarcodeRegex {
+    /// Regex pattern to parse barcode(s) from read sequence
     regex: Regex,
+
+    /// List of barcode types parsed from provided pattern
     barcode_types: Vec<BarcodeType>,
 }
 
 impl BarcodeRegex {
+    /// Creates `BarcodeRegex` instance
+    /// 
+    /// Example
+    /// ```
+    /// use barkit_extract::pattern::BarcodeRegex;
+    /// 
+    /// let barcode_regex = BarcodeRegex::new("^atgc(?<UMI>[ATGCN]{6})", 1);
+    /// ```
     pub fn new(pattern: &str, max_error: usize) -> Result<Self, Error> {
         let barcode_pattern = BarcodePattern::new(pattern, &max_error)?;
         let fuzzy_pattern = barcode_pattern.get_pattern_with_errors()?;
@@ -156,13 +184,7 @@ impl BarcodeRegex {
         })
     }
 
-    pub fn get_captures<'a>(&'a self, read_seq: &'a [u8]) -> Result<Captures, Error> {
-        match self.regex.captures(read_seq) {
-            Some(capture) => Ok(capture),
-            None => Err(Error::PatternNotMatched),
-        }
-    }
-
+    /// Parses capture groups from regex pattern 
     fn parse_capture_groups(regex: &Regex) -> Result<Vec<BarcodeType>, Error> {
         let mut capture_groups = Vec::<BarcodeType>::new();
         for capture_group in regex
@@ -171,12 +193,37 @@ impl BarcodeRegex {
             .into_iter()
             .flatten()
         {
-            capture_groups.push(BarcodeType::get_barcode_type(capture_group)?)
+            capture_groups.push(BarcodeType::parse_type(capture_group)?)
         }
         if capture_groups.is_empty() {
             return Err(Error::BarcodeCaptureGroupNotFound(regex.to_string()));
         }
         Ok(capture_groups)
+    }
+
+    /// Captures barcodes in read sequence
+    /// 
+    /// Example
+    /// ```
+    /// use barkit_extract::pattern::BarcodeRegex;
+    /// 
+    /// let barcode_regex = BarcodeRegex::new("^atgc(?<UMI>[ATGCN]{6})", 1).unwrap();
+    /// 
+    /// assert_eq!(
+    ///     b"NNNNNN",
+    ///     barcode_regex
+    ///         .get_captures(b"ATGCNNNNNNCCC")
+    ///         .unwrap()
+    ///         .name("UMI")
+    ///         .unwrap()
+    ///         .as_bytes()
+    /// );
+    /// ```
+    pub fn get_captures<'a>(&'a self, read_seq: &'a [u8]) -> Result<Captures, Error> {
+        match self.regex.captures(read_seq) {
+            Some(capture) => Ok(capture),
+            None => Err(Error::PatternNotMatched),
+        }
     }
 
     pub fn get_barcode_types(&self) -> Vec<BarcodeType> {
